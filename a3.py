a51b4390649e2a87d1589b62396891c74aa2baa2
MichelleJong <57374658+MichelleJong@users.noreply.github.com>
2020-11-16 22:07:03 +1100

    Update a3.py

--- a3.py
+++ a3.py
@@ -1,61 +1,55 @@
 from microbit import *
+from gesture import *
 import neopixel
-import music
 from random import randint
-#from gesture import *
-
-# libraries
-#gesture = Gesture()
-
+import music
+gesture_sensor = Gesture()
 np = neopixel.NeoPixel(pin1, 10)
 
-#Initial
-display.show(Image.ARROW_W)
-#Timer portion - Gesture - swipe left to start timer
-#After timer starts the display changes to happy face
-#Half of the time over -> Sad face
+display.show(Image.ARROW_S)
 num_leds = 10
 for i in range(num_leds):
     np[i] = (0, 255, 0)
 np.show()
 
 while True:
-    for i in range(num_leds):
-        np[i] = (255, 0, 0)
-        np.show()
-        sleep(1000)
+    g = gesture_sensor.read()
 
-#Music should play after the LEDs cycle once?
-#How to end loop??
-    if i == 10:
-        np.clear()
-        music.play(music.RINGTONE)
-        music.stop()
+    if g == 'down':
+        display.show(Image.SMILE)
+        for i in range(num_leds):
+            np[i] = (255, 0, 0)
+            sleep(1000)
+            np.show()
+            if i == 10:
+                music.play(music.RINGTONE)
 
-#If task completed successfully (A button):
-music.play(music.POWER_UP)
-
-while True:
-    for pixel_id in range(0, len(np)):
-        red = randint(40, 100)
-        green = randint(100, 255)
-        blue = randint(0, 40)
-
-        np[pixel_id] = (red, green, blue)
+    elif g == 'up':
+        for i in range(num_leds):
+            np[i] = (0, 255, 0)
+            np.show()
+    sleep(100)
+    
+    if button_a.is_pressed():
+        display.show(Image.HAPPY)
+        music.play(music.POWER_UP)
+        for i in range(num_leds):
+            red = randint(40, 100)
+            green = randint(100, 255)
+            blue = randint(0, 40)
+            np[i] = (red, green, blue)
 
         np.show()
         sleep(50)
 
-#If task completed unsuccessfully (B Button):
-    music.play(music.POWER_DOWN)
-
-while True:
-    for pixel_id in range(0, len(np)):
-        red = randint(100, 255)
-        green = randint(0, 40)
-        blue = randint(0, 40)
-
-        np[pixel_id] = (red, green, blue)
+    if button_b.is_pressed():
+        display.show(Image.SAD)
+        music.play(music.POWER_DOWN)
+        for i in range(num_leds):
+            red = randint(100, 255)
+            green = randint(0, 40)
+            blue = randint(0, 40)
+            np[i] = (red, green, blue)
 
         np.show()
         sleep(50)
